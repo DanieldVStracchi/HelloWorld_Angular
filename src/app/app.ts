247d3cecd4038dbb1ac0e46bc1614b69d373f191
DanieldVStracchi <daniel.devicentestracchi@reliefapplications.com>
2026-06-16 10:24:46 +0200

    FIRST CHANGE

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   imports: [],
-  template: ` <h1>HELLO BOBEH</h1> `,
+  template: ` <h1>HELLO UNIVERSE</h1> `,
   styleUrls: ['./app.css'],
 })
 export class App {
